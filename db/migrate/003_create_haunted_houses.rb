581da28a91fd7fd943e61473ed8f9677030c7534
Reno Sorensen <renojames19@gmail.com>
2019-04-24 04:24:58 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,14 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 
   def change
     create_table :haunted_houses do |t|
-
+      t.text :name
+      t.text :location
+      t.text :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :long_description
     end
   end
 
